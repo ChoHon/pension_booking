812f1f4370e17b08a3e0b1e6f050673276c3523b
chohon <siaksiakx@gmail.com>
2023-12-09 00:27:08 +0900

    Pension ViewSet으로 Read, Update, Delete 구현

--- server/pensions/models.py
+++ server/pensions/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class Pension(BaseModel):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=30)
     address = models.CharField(max_length=100, blank=True)
     phone_number = models.CharField(max_length=11, blank=True)
--- server/pensions/views.py
+++ server/pensions/views.py
@@ -8,5 +8,32 @@ class PensionViewSet(viewsets.ModelViewSet):
     serializer_class = PensionSerializer
 
     def create(self, request, *args, **kwargs):
-        request.data["owner"] = request.user.pk
+        request.data["user"] = request.user.pk
         return super().create(request, *args, **kwargs)
+    
+    def list(self, request, *args, **kwargs):
+        self.queryset = self.queryset.filter(user=request.user)
+
+        serializer = self.get_serializer(self.queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.queryset.get(id=kwargs["pk"], user=request.user)
+
+        serializer = PensionSerializer(instance)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def partial_update(self, request, *args, **kwargs):
+        instance = self.queryset.get(id=kwargs["pk"], user=request.user)
+
+        serializer = PensionSerializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def destroy(self, request, *args, **kwargs):
+        instance = self.queryset.get(id=kwargs["pk"], user=request.user)
+        self.perform_destroy(instance)
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
