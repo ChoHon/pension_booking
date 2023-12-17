45c355a2c577f634c9ab884139f6ee847508514a
chohon <siaksiakx@gmail.com>
2023-12-18 01:01:23 +0900

    Front 회원가입 View 수정

--- frontend/src/views/SignupView.vue
+++ frontend/src/views/SignupView.vue
@@ -1,111 +1,119 @@
 <template>
-  <section class="bg-gray-50">
-    <div
-      class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0"
-    >
-      <div
-        class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0"
+  <div
+    class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8"
+  >
+    <!-- 타이틀 -->
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+      <h2
+        class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"
       >
-        <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-          <!-- 헤더 -->
-          <h1
-            class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl"
+        회원가입
+      </h2>
+    </div>
+
+    <!-- 폼 -->
+    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <form class="space-y-6">
+        <!-- 이메일 -->
+        <div>
+          <label
+            for="email"
+            class="block text-md font-medium leading-6 text-gray-900"
+          >
+            이메일
+          </label>
+          <div class="mt-2">
+            <input
+              v-model="new_user.email"
+              id="email"
+              name="email"
+              type="email"
+              autocomplete="email"
+              required
+              class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+
+        <!-- 비밀번호 -->
+        <div>
+          <div class="flex items-center justify-between">
+            <label
+              for="password"
+              class="block text-md font-medium leading-6 text-gray-900"
+            >
+              비밀번호
+            </label>
+            <!-- <div class="text-sm">
+              <a
+                href="#"
+                class="font-semibold text-indigo-600 hover:text-indigo-500"
+              >
+                Forgot password?
+              </a>
+            </div> -->
+          </div>
+          <div class="mt-2">
+            <input
+              v-model="new_user.password"
+              id="password"
+              name="password"
+              type="password"
+              autocomplete="current-password"
+              required
+              class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+
+        <!-- 비밀번호 확인 -->
+        <div>
+          <div class="flex items-center justify-between">
+            <label
+              for="confirm_password"
+              class="block text-md font-medium leading-6 text-gray-900"
+            >
+              비밀번호 확인
+            </label>
+          </div>
+          <div class="mt-2">
+            <input
+              v-model="new_user.confirm_password"
+              id="confirm_password"
+              name="confirm_password"
+              type="password"
+              autocomplete="current-confirm_password"
+              required
+              :class="[
+                new_user.password === new_user.confirm_password ||
+                new_user.confirm_password === ''
+                  ? 'bg-white'
+                  : 'bg-red-100',
+                'block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6',
+              ]"
+            />
+          </div>
+        </div>
+
+        <!-- 로그인 버튼 -->
+        <div>
+          <button
+            type="button"
+            class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            @click="signup"
           >
             회원가입
-          </h1>
-
-          <form class="space-y-4 md:space-y-6" action="#">
-            <!-- 이메일 -->
-            <div>
-              <label
-                for="email"
-                class="block mb-2 text-md font-medium text-gray-900"
-              >
-                이메일
-              </label>
-              <input
-                v-model="new_user.email"
-                type="email"
-                name="email"
-                id="email"
-                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-                placeholder="name@company.com"
-                required
-              />
-            </div>
-
-            <!-- 비밀번호 -->
-            <div>
-              <label
-                for="password"
-                class="block mb-2 text-md font-medium text-gray-900"
-              >
-                비밀번호
-              </label>
-              <input
-                v-model="new_user.password"
-                type="password"
-                name="password"
-                id="password"
-                placeholder="••••••••"
-                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-                required
-              />
-            </div>
-
-            <!-- 비밀번호 확인 -->
-            <div>
-              <label
-                for="confirm-password"
-                class="block mb-2 text-md font-medium text-gray-900"
-                >비밀번호 확인</label
-              >
-              <input
-                v-model="new_user.confirm_password"
-                type="password"
-                name="confirm-password"
-                id="confirm-password"
-                placeholder="••••••••"
-                :class="{
-                  'bg-red-100':
-                    new_user.password !== new_user.confirm_password &&
-                    new_user.confirm_password.length > 0,
-                }"
-                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-                required
-              />
-            </div>
-
-            <!-- 회원가입 버튼 -->
-            <button
-              type="submit"
-              class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-md px-5 py-2.5 text-center"
-              @click.prevent="signup"
-            >
-              회원가입
-            </button>
-
-            <!-- 로그인 화면 이동 -->
-            <p class="text-sm font-light text-gray-500">
-              이미 회원이신가요?
-              <RouterLink :to="{ name: 'login' }">
-                <span
-                  class="font-medium text-primary-600 hover:underline cursor-pointer"
-                >
-                  로그인
-                </span>
-              </RouterLink>
-            </p>
-          </form>
+          </button>
         </div>
-      </div>
+      </form>
     </div>
-  </section>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { reactive } from 'vue';
 import http from '@/services/http.js';
+import router from '@/router';
 
 const new_user = reactive({
   email: '',
@@ -113,17 +121,15 @@ const new_user = reactive({
   confirm_password: '',
 });
 
-const signup = () => {
-  const response = http
-    .post('/accounts/signup/', new_user)
-    .then(res => {
-      console.log(res.data);
-    })
-    .catch(err => {
-      console.log(err.response.data);
-    });
-  return response;
+const signup = async () => {
+  try {
+    const response = await http.post('/accounts/signup/', new_user);
+    alert('회원가입이 완료되었습니다.');
+    router.push('/login');
+
+    return response;
+  } catch (error) {
+    console.log(error.response);
+  }
 };
 </script>
-
-<style scoped></style>
