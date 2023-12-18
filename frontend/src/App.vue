fc82c0b760671be45caab0a7cca2bb96f430b545
chohon <siaksiakx@gmail.com>
2023-12-18 20:01:06 +0900

    Front RouterView 레이아웃 설정

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,20 @@
 <template>
-  <div class="flex flex-col min-h-screen font-sans bg-gray-50">
+  <div class="min-h-full">
     <SiteNavigation />
-    <RouterView />
+
+    <!-- <header class="bg-white shadow">
+      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <h1 class="text-3xl font-bold tracking-tight text-gray-900">
+          Dashboard
+        </h1>
+      </div>
+    </header> -->
+
+    <main>
+      <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <RouterView />
+      </div>
+    </main>
   </div>
 </template>
 
