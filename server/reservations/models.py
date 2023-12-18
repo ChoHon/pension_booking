066363ce52387b62e0295de62f15ecadaf8f4df0
chohon <siaksiakx@gmail.com>
2023-12-18 19:37:39 +0900

    Server Reservation Model reservation_date 필드 추가

--- server/reservations/models.py
+++ server/reservations/models.py
@@ -4,8 +4,11 @@ from util.base_models import BaseModel
 
 class Reservation(BaseModel):
     room = models.ForeignKey('rooms.room', on_delete=models.CASCADE)
+    reservation_date = models.DateField()
+
     option_bbq = models.BooleanField()
     option_over_capacity = models.BooleanField()
+
     discount = models.IntegerField()
     final_payment = models.IntegerField()
 
