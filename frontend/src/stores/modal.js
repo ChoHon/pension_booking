3001ee106e85170aa1ec6638a26ace583be30682
chohon <siaksiakx@gmail.com>
2023-12-31 23:00:36 +0900

    Front 방 수정 API 연동

--- frontend/src/stores/modal.js
+++ frontend/src/stores/modal.js
@@ -3,10 +3,15 @@ import { ref } from 'vue';
 
 export const useModalStore = defineStore('modal', () => {
   const target_pension = ref([]);
+  const target_room = ref([]);
 
   const setTargetPension = input => {
     target_pension.value = input;
   };
 
-  return { target_pension, setTargetPension };
+  const setTargetRoom = input => {
+    target_room.value = input;
+  };
+
+  return { target_pension, target_room, setTargetPension, setTargetRoom };
 });
--- frontend/src/views/pensions/PensionView.vue
+++ frontend/src/views/pensions/PensionView.vue
@@ -63,16 +63,6 @@
                     방 추가
                   </div>
                 </MenuItem>
-                <MenuItem v-slot="{ active }">
-                  <div
-                    :class="[
-                      active ? 'bg-gray-50' : '',
-                      'block px-3 py-1 text-sm leading-6 text-gray-900 cursor-pointer',
-                    ]"
-                  >
-                    방 삭제
-                  </div>
-                </MenuItem>
               </div>
               <div class="py-1">
                 <MenuItem v-slot="{ active }">
@@ -109,7 +99,50 @@
           v-for="room in pension.rooms"
           class="flex justify-between gap-x-4 py-3"
         >
-          <dt class="text-gray-500">{{ room.name }}</dt>
+          <dt class="text-gray-500">
+            <Menu as="div" class="relative ml-auto">
+              <MenuButton class="-m-2.5 z-10 block p-2.5 hover:text-gray-700">
+                {{ room.name }}
+              </MenuButton>
+              <transition
+                enter-active-class="transition ease-out duration-100"
+                enter-from-class="transform opacity-0 scale-95"
+                enter-to-class="transform opacity-100 scale-100"
+                leave-active-class="transition ease-in duration-75"
+                leave-from-class="transform opacity-100 scale-100"
+                leave-to-class="transform opacity-0 scale-95"
+              >
+                <MenuItems
+                  class="absolute right-0 z-20 mt-0.5 w-32 origin-top-right divide-y divide-gray-100 rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none"
+                >
+                  <div class="py-1">
+                    <MenuItem v-slot="{ active }">
+                      <div
+                        :class="[
+                          active ? 'bg-gray-50' : '',
+                          'block px-3 py-1 text-sm leading-6 text-gray-900 cursor-pointer',
+                        ]"
+                        @click="openRoomModModal(room)"
+                      >
+                        수정
+                      </div>
+                    </MenuItem>
+                    <MenuItem v-slot="{ active }">
+                      <div
+                        :class="[
+                          active ? 'bg-gray-50' : '',
+                          'block px-3 py-1 text-sm leading-6 text-gray-900 cursor-pointer',
+                        ]"
+                        @click=""
+                      >
+                        삭제
+                      </div>
+                    </MenuItem>
+                  </div>
+                </MenuItems>
+              </transition>
+            </Menu>
+          </dt>
           <dd class="text-gray-700">
             <div>{{ room.capacity }}인실 - {{ room.price_peak_season }}원</div>
           </dd>
@@ -118,6 +151,7 @@
     </li>
   </ul>
 
+  <!-- 펜션 추가 -->
   <BaseModal
     :is_open="is_pension_add_modal_open"
     @close="is_pension_add_modal_open = false"
@@ -186,6 +220,7 @@
     </template>
   </BaseModal>
 
+  <!-- 펜션 수정 -->
   <BaseModal
     :is_open="is_pension_mod_modal_open"
     @close="is_pension_mod_modal_open = false"
@@ -256,6 +291,7 @@
     </template>
   </BaseModal>
 
+  <!-- 방 추가 -->
   <BaseModal
     :is_open="is_room_add_modal_open"
     @close="is_room_add_modal_open = false"
@@ -364,6 +400,116 @@
       </button>
     </template>
   </BaseModal>
+
+  <!-- 방 수정 -->
+  <BaseModal
+    :is_open="is_room_mod_modal_open"
+    @close="is_room_mod_modal_open = false"
+  >
+    <template #header>방 수정</template>
+    <template #body>
+      <form class="space-y-6">
+        <div>
+          <label
+            for="name"
+            class="block text-sm font-medium leading-6 text-gray-900"
+          >
+            이름
+          </label>
+          <div class="mt-2">
+            <input
+              v-model="new_room.name"
+              id="name"
+              name="name"
+              type="text"
+              autocomplete="name"
+              required
+              class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+
+        <div>
+          <div class="flex items-center justify-between">
+            <label
+              for="capacity"
+              class="block text-sm font-medium leading-6 text-gray-900"
+            >
+              수용 인원
+            </label>
+          </div>
+          <div class="mt-2">
+            <input
+              v-model="new_room.capacity"
+              id="capacity"
+              name="capacity"
+              type="number"
+              required
+              class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+
+        <div>
+          <div class="flex items-center justify-between">
+            <label
+              for="price_peak_season"
+              class="block text-sm font-medium leading-6 text-gray-900"
+            >
+              성수기 가격
+            </label>
+          </div>
+          <div class="mt-2">
+            <input
+              v-model="new_room.price_peak_season"
+              id="price_peak_season"
+              name="price_peak_season"
+              type="number"
+              required
+              class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+
+        <div>
+          <div class="flex items-center justify-between">
+            <label
+              for="price_off_season"
+              class="block text-sm font-medium leading-6 text-gray-900"
+            >
+              비수기 가격
+            </label>
+          </div>
+          <div class="mt-2">
+            <input
+              v-model="new_room.price_off_season"
+              id="price_off_season"
+              name="price_off_season"
+              type="number"
+              required
+              class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+      </form>
+    </template>
+    <template #buttons>
+      <button
+        type="button"
+        class="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:col-start-2"
+        @click="modifyRoom"
+      >
+        수정
+      </button>
+      <button
+        type="button"
+        class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0"
+        @click="is_room_mod_modal_open = false"
+      >
+        취소
+      </button>
+    </template>
+  </BaseModal>
 </template>
 
 <script setup lang="ts">
@@ -382,6 +528,7 @@ const modalStore = useModalStore();
 const is_pension_add_modal_open = ref(false);
 const is_pension_mod_modal_open = ref(false);
 const is_room_add_modal_open = ref(false);
+const is_room_mod_modal_open = ref(false);
 
 const openPensionModModal = pension => {
   is_pension_mod_modal_open.value = true;
@@ -391,6 +538,11 @@ const openPensionModModal = pension => {
 const openRoomAddModal = pension => {
   is_room_add_modal_open.value = true;
   modalStore.setTargetPension(pension);
+};
+
+const openRoomModModal = room => {
+  is_room_mod_modal_open.value = true;
+  modalStore.setTargetRoom(room);
 };
 
 // 펜션 목록 불러오기
@@ -413,7 +565,6 @@ const new_pension = reactive({
   name: '',
   address: '',
 });
-
 const addPension = async () => {
   try {
     const response = await http.post('/pensions/', new_pension, {
@@ -475,9 +626,9 @@ const removePension = async (pension_id: number) => {
 // 방 추가
 const new_room = reactive({
   name: '',
-  capacity: 0,
-  price_peak_season: 0,
-  price_off_season: 0,
+  capacity: null,
+  price_peak_season: null,
+  price_off_season: null,
 });
 const addRoom = async () => {
   try {
@@ -494,7 +645,37 @@ const addRoom = async () => {
       }
     );
     alert('방이 추가되었습니다.');
-    console.log(response.data);
+  } catch (error) {
+    console.log(error.response.data);
+  }
+
+  router.go(0);
+  return null;
+};
+
+// 방 수정
+const modifyRoom = async () => {
+  try {
+    const request = {};
+    new_room.name ? (request['name'] = new_room.name) : null;
+    new_room.capacity ? (request['capacity'] = new_room.capacity) : null;
+    new_room.price_peak_season
+      ? (request['price_peak_season'] = new_room.price_peak_season)
+      : null;
+    new_room.price_off_season
+      ? (request['price_off_season'] = new_room.price_off_season)
+      : null;
+
+    const response = await http.patch(
+      `rooms/${modalStore.target_room.id}/`,
+      request,
+      {
+        headers: {
+          Authorization: `Bearer ${authStore.access}`,
+        },
+      }
+    );
+    alert('방이 수정되었습니다.');
   } catch (error) {
     console.log(error.response.data);
   }
